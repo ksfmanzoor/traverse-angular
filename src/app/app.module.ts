a8397f726da1e596719998eeac7f63b60a6f835d
ChHannan <45265763+ChHannan@users.noreply.github.com>
2020-02-23 00:37:45 +0500

    Added Destination Images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { DestinationsComponent } from './destinations/destinations.component';
+import { DestinationImageComponent } from './destination-image/destination-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DestinationsComponent,
+    DestinationImageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/destination-image/destination-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-image',
+  templateUrl: './destination-image.component.html',
+  styleUrls: ['./destination-image.component.css']
+})
+export class DestinationImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
