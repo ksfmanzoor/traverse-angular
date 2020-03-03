5a4478a09ebe14950b47efe3a2f5905af8721b2f
ChHannan <45265763+ChHannan@users.noreply.github.com>
2020-03-03 22:20:59 +0500

    Added owl carousel

--- src/app/Trip Page/places/places.component.ts
+++ src/app/Trip Page/places/places.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {OwlOptions} from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-places',
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./places.component.css']
 })
 export class PlacesComponent implements OnInit {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    freeDrag: true,
+    navSpeed: 700,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    rtl: true
+  };
 
   constructor() { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './Home Page/header/header.component';
-import { DestinationsComponent } from './Home Page/destinations/destinations.component';
-import { DestinationImageComponent } from './Home Page/destination-image/destination-image.component';
-import { ExperiencesComponent } from './Home Page/experiences/experiences.component';
-import { BestTripsComponent } from './Home Page/best-trips/best-trips.component';
-import { FooterComponent } from './footer/footer.component';
-import { TripPageComponent } from './Trip Page/trip-page/trip-page.component';
-import { AboutUsComponent } from './Trip Page/about-us/about-us.component';
-import { PlacesComponent } from './Trip Page/places/places.component';
-import { GalleryComponent } from './Trip Page/gallery/gallery.component';
-import { GalleryImageComponent } from './Trip Page/gallery-image/gallery-image.component';
-import { HomePageComponent } from './Home Page/home-page/home-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './Home Page/header/header.component';
+import {DestinationsComponent} from './Home Page/destinations/destinations.component';
+import {DestinationImageComponent} from './Home Page/destination-image/destination-image.component';
+import {ExperiencesComponent} from './Home Page/experiences/experiences.component';
+import {BestTripsComponent} from './Home Page/best-trips/best-trips.component';
+import {FooterComponent} from './footer/footer.component';
+import {TripPageComponent} from './Trip Page/trip-page/trip-page.component';
+import {AboutUsComponent} from './Trip Page/about-us/about-us.component';
+import {PlacesComponent} from './Trip Page/places/places.component';
+import {GalleryComponent} from './Trip Page/gallery/gallery.component';
+import {GalleryImageComponent} from './Trip Page/gallery-image/gallery-image.component';
+import {HomePageComponent} from './Home Page/home-page/home-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CarouselModule} from 'ngx-owl-carousel-o';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    DestinationsComponent,
-    DestinationImageComponent,
-    ExperiencesComponent,
-    BestTripsComponent,
-    FooterComponent,
-    TripPageComponent,
-    AboutUsComponent,
-    PlacesComponent,
-    GalleryComponent,
-    GalleryImageComponent,
-    HomePageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        DestinationsComponent,
+        DestinationImageComponent,
+        ExperiencesComponent,
+        BestTripsComponent,
+        FooterComponent,
+        TripPageComponent,
+        AboutUsComponent,
+        PlacesComponent,
+        GalleryComponent,
+        GalleryImageComponent,
+        HomePageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        CarouselModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
