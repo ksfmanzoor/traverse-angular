80c6a26d1d9b88143df8465afa392602eda669dc
Ch.Hannan <ch.hannan15@gmail.com>
2020-03-22 22:00:57 +0500

    Added carousel navigation

--- src/app/trip-page/places/places.component.ts
+++ src/app/trip-page/places/places.component.ts
@@ -1,41 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {OwlOptions} from 'ngx-owl-carousel-o';
 
 @Component({
-  selector: 'app-places',
-  templateUrl: './places.component.html',
-  styleUrls: ['./places.component.css']
+    selector: 'app-places',
+    templateUrl: './places.component.html',
+    styleUrls: ['./places.component.css']
 })
 export class PlacesComponent implements OnInit {
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    freeDrag: true,
-    navSpeed: 700,
-    dots: false,
-    autoplay: true,
-    rtl: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
-    },
-  };
+    customOptions: OwlOptions = {
+        loop: true,
+        mouseDrag: true,
+        touchDrag: true,
+        pullDrag: true,
+        navSpeed: 700,
+        dots: false,
+        autoplay: true,
+        rtl: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            400: {
+                items: 2
+            },
+            740: {
+                items: 3
+            },
+            940: {
+                items: 4
+            }
+        },
+    };
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
