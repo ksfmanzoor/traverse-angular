6a7262aaa08a085efc7e5f27a7a91fe9b899bfc8
CH.Hannan <ch.hannan15@gmail.com>
2020-05-28 19:44:52 +0500

    Completed login

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -3,44 +3,55 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthenticationService} from '../services/authentication.service';
 
 @Component({
-    selector: 'app-login-page',
-    templateUrl: './login-page.component.html',
-    styleUrls: ['./login-page.component.css']
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-    requiredInfo = {heading: 'Sign In', subtitle: 'Log in to your account', altText: 'Don\'t', route: '/signup', keyWord: 'Sign Up'};
-    loginForm: FormGroup;
-    loginData = {};
-    isPhone = false;
+  requiredInfo = {
+    heading: 'Sign In',
+    subtitle: 'Log in to your account',
+    altText: 'Don\'t',
+    route: '/signup',
+    keyWord: 'Sign Up'
+  };
+  loginForm: FormGroup;
+  loginData = {};
+  isPhone = false;
 
-    constructor(private authenticationService: AuthenticationService) {
+  constructor(private authenticationService: AuthenticationService) {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      phoneNumber: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  get formControl() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    if (this.isPhone) {
+      this.loginData = {
+        phone_number: this.formControl.phoneNumber.value,
+        password: this.formControl.password.value
+      };
+    } else {
+      this.loginData = {email: this.formControl.email.value, password: this.formControl.password.value};
+
     }
+    this.authenticationService.login(this.loginData);
+  }
 
-    ngOnInit(): void {
-        this.loginForm = new FormGroup({
-            phoneNumber: new FormControl(null, [Validators.required]),
-            email: new FormControl(null, [Validators.required, Validators.email]),
-            password: new FormControl(null, [Validators.required, Validators.minLength(6)])
-        });
-    }
+  onEmailClick() {
+    this.isPhone = false;
+  }
 
-    get formControl() { return this.loginForm.controls; }
-
-    onSubmit() {
-        if (this.isPhone) {
-            this.loginData = {phone_number: this.formControl.phoneNumber.value, password: this.formControl.password.value};
-        } else {
-            this.loginData = {email: this.formControl.email.value, password: this.formControl.password.value};
-
-        }
-        this.authenticationService.login(this.loginData);
-    }
-
-    onEmailClick() {
-        this.isPhone = false;
-    }
-
-    onPhoneClick() {
-        this.isPhone = true;
-    }
+  onPhoneClick() {
+    this.isPhone = true;
+  }
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -7,75 +7,75 @@ import {AuthService, GoogleLoginProvider} from 'angularx-social-login';
 import {Router} from '@angular/router';
 
 export interface Token {
-    token: string;
+  token: string;
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthenticationService {
-    private baseUrl = 'http://traverse.ap-south-1.elasticbeanstalk.com/api/';
-    token = '';
-    private currentUserSubject: BehaviorSubject<any>;
-    public currentUser: Observable<User>;
+  private baseUrl = 'http://traverse.ap-south-1.elasticbeanstalk.com/api/';
+  token = '';
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<User>;
 
-    constructor(private httpClient: HttpClient, private authService: AuthService, private router: Router) {
-        this.currentUserSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('currentUser')));
-        this.currentUser = this.currentUserSubject.asObservable();
-    }
+  constructor(private httpClient: HttpClient, private authService: AuthService, private router: Router) {
+    this.currentUserSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
-    signUp(signUpData) {
-        return this.httpClient.post(this.baseUrl + 'user/', signUpData);
-    }
+  signUp(signUpData) {
+    return this.httpClient.post(this.baseUrl + 'user/', signUpData);
+  }
 
-    login(loginData) {
-        return this.httpClient.post(this.baseUrl + 'token/', loginData).pipe(map((tokenObject: Token) => {
-            this.token = tokenObject.token;
-        }), mergeMap(data => this.httpClient.get(this.baseUrl + 'user/'))).subscribe(user => {
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            this.currentUserSubject.next(user);
-            this.router.navigate(['/']).then();
-            return user;
-        });
-    }
+  login(loginData) {
+    return this.httpClient.post(this.baseUrl + 'token/', loginData).pipe(map((tokenObject: Token) => {
+      this.token = tokenObject.token;
+    }), mergeMap(data => this.httpClient.get(this.baseUrl + 'user/'))).subscribe(user => {
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      this.currentUserSubject.next(user);
+      this.router.navigate(['/']).then();
+      return user;
+    });
+  }
 
-    logout() {
-        localStorage.removeItem('currentUser');
-        this.currentUserSubject.next(null);
-    }
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
 
-    signInWithGoogle() {
-        this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then((data) => {
-            this.httpClient.post(this.baseUrl + 'user/', {
-                email: data.email,
-                password: data.idToken.substring(0, 30),
-                name: data.name,
-                google_token: data.idToken,
-                is_google_auth: true
-            }).subscribe(user => {
-                this.login({email: data.email, password: data.idToken.substring(0, 30)});
-            }, error => {
-                if (error.error.detail === 'User with the same credentials already exists!') {
-                    this.login({email: data.email, password: data.idToken.substring(0, 30)});
-                }
-            });
-        }).catch(error => {
-            alert(error);
-        });
-    }
+  signInWithGoogle() {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then((data) => {
+      this.httpClient.post(this.baseUrl + 'user/', {
+        email: data.email,
+        password: data.idToken.substring(0, 30),
+        name: data.name,
+        google_token: data.idToken,
+        is_google_auth: true
+      }).subscribe(user => {
+        this.login({email: data.email, password: data.idToken.substring(0, 30)});
+      }, error => {
+        if (error.error.detail === 'User with the same credentials already exists!') {
+          this.login({email: data.email, password: data.idToken.substring(0, 30)});
+        }
+      });
+    }).catch(error => {
+      alert(error);
+    });
+  }
 
-    resetPasswordRequest(emailAddress: string) {
-        return this.httpClient.post(this.baseUrl + 'reset/password/request/',
-            {email: emailAddress});
-    }
+  resetPasswordRequest(emailAddress: string) {
+    return this.httpClient.post(this.baseUrl + 'reset/password/request/',
+      {email: emailAddress});
+  }
 
-    resetPasswordConfirm(resetToken: string) {
-        return this.httpClient.post(this.baseUrl + 'reset/password/confirm/',
-            {reset_token: resetToken});
-    }
+  resetPasswordConfirm(resetToken: string) {
+    return this.httpClient.post(this.baseUrl + 'reset/password/confirm/',
+      {reset_token: resetToken});
+  }
 
-    resetPassword(resetToken: string, newPassword: string) {
-        return this.httpClient.post(this.baseUrl + 'reset/password/',
-            {reset_token: resetToken, password: newPassword});
-    }
+  resetPassword(resetToken: string, newPassword: string) {
+    return this.httpClient.post(this.baseUrl + 'reset/password/',
+      {reset_token: resetToken, password: newPassword});
+  }
 }
