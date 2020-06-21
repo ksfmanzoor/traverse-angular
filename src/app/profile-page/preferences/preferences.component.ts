8d6f123d2077764a3f775d20489e2bbaa18e23a9
CH.Hannan <ch.hannan15@gmail.com>
2020-06-22 00:46:12 +0500

    Completed preference page

--- src/app/profile-page/preferences/preferences.component.ts
+++ src/app/profile-page/preferences/preferences.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faPencilAlt} from '@fortawesome/free-solid-svg-icons/faPencilAlt';
 
 @Component({
   selector: 'app-preferences',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preferences.component.css']
 })
 export class PreferencesComponent implements OnInit {
+  pencilIcon = faPencilAlt;
 
   constructor() { }
 
