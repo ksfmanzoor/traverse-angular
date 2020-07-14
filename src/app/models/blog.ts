0b9009a819f53496a0739e0b8e24c7c0029d2a54
CH.Hannan <ch.hannan15@gmail.com>
2020-07-14 12:32:47 +0500

    Updated publish text

--- src/app/models/blog.ts
+++ src/app/models/blog.ts
@@ -1,18 +1,19 @@
 export interface Blog {
-    id: string;
-    slug: string;
-    title: string;
-    keywords: Keywords[];
-    content: string;
-    user: string;
-    tags: Tag[];
-    thumbnail: string;
-    subtitle: string;
+  id: string;
+  slug: string;
+  title: string;
+  keywords: Keywords[];
+  content: string;
+  user: string;
+  tags: Tag[];
+  thumbnail: string;
+  subtitle: string;
+  is_published: boolean;
 }
 
 export interface Tag {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 }
 
 export interface Keywords {
